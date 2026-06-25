eb003a3dbc3c6e608a99ab3dd8446006a89f8e23
minssajang <minssajang@gmail.com>
2026-06-25 19:55:18 +0900

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['google-auth-library', 'google-logging-utils', 'gcp-metadata'],
+  experimental: {
+    serverComponentsExternalPackages: ['google-auth-library', 'google-logging-utils', 'gcp-metadata'],
+  },
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'plus.unsplash.com'],
